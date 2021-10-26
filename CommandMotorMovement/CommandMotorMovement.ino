025200aa1b77eeb0cebe60c6e6b97cc87e667d02
SimonHaaningAAU <san20@student.aau.dk>
2021-10-26 13:45:45 +0200

    Working on torque limiting

--- CommandMotorMovement/CommandMotorMovement.ino
+++ CommandMotorMovement/CommandMotorMovement.ino
@@ -7,23 +7,38 @@ const uint8_t DXL_ID[6] = {0, 1, 2, 3, 4, 5};
 const float DXL_PROTOCOL_VERSION = 2.0;
 DynamixelShield dxl;
 
+//Namespace for motor control table entries
+using namespace ControlTableItem;
+
 void setup() {
   // put your setup code here, to run once:
-
-  //Set baudrate of DYNAMIXEL port
-  dxl.begin(57600);
 
   // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
   dxl.begin(57600);
   // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
+  //Set maximum torque (percentage)
+  setTorqueLimit(DXL_ID[1], 0);
+  setTorqueLimit(DXL_ID[2], 100);
+  setTorqueLimit(DXL_ID[3], 0);
+  setTorqueLimit(DXL_ID[4], 0);
+  setTorqueLimit(DXL_ID[5], 0);
+
+  //Orient arm
+  startupPosition(DXL_ID[2]);
+  startupPosition(DXL_ID[3]);
+  dxl.setGoalPosition(DXL_ID[2], 4095/2);
+  //dxl.setGoalPosition(DXL_ID[3], 4095/2);
+
+  //delay(1000);
+  
   //Initialise motor control modes
-  startupCurrent(DXL_ID[1]);
-  startupCurrent(DXL_ID[2]);
-  startupCurrent(DXL_ID[3]);
-  startupPosition(DXL_ID[4]);
-  startupPosition(DXL_ID[5]);
+  //startupCurrent(DXL_ID[1]);
+  //startupCurrent(DXL_ID[2]);
+  //startupCurrent(DXL_ID[3]);
+  //startupPosition(DXL_ID[4]);
+  //startupPosition(DXL_ID[5]);
 }
 
 void loop() {
