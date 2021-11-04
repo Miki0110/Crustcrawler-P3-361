d177106b515c225ac45c31a485e350ff2bb6fd85
SimonHaaningAAU <san20@student.aau.dk>
2021-11-04 13:29:04 +0100

    Added currentposition mode to startupmodes

--- CommandMotorMovement/startup.ino
+++ CommandMotorMovement/startup.ino
@@ -1,20 +1,31 @@
+//MX-106 and MX-64
 boolean startupCurrent(uint8_t id){
   dxl.ping(id);
   dxl.torqueOff(id);
-  dxl.setOperatingMode(id, OP_CURRENT);
+  dxl.setOperatingMode(id, 0);
   dxl.torqueOn(id);
 }
 
+//MX-106, MX-64 and MX-28
 boolean startupVelocity(uint8_t id){
   dxl.ping(id);
   dxl.torqueOff(id);
-  dxl.setOperatingMode(id, OP_VELOCITY);
+  dxl.setOperatingMode(id, 1);
   dxl.torqueOn(id);
 }
 
+//MX-106, MX-64 and MX-28
 boolean startupPosition(uint8_t id){
   dxl.ping(id);
   dxl.torqueOff(id);
-  dxl.setOperatingMode(id, OP_POSITION);
+  dxl.setOperatingMode(id, 3);
+  dxl.torqueOn(id);
+}
+
+//MX-106 and MX-64
+boolean startupCurrentPosition(uint8_t id){
+  dxl.ping(id);
+  dxl.torqueOff(id);
+  dxl.setOperatingMode(id, 5);
   dxl.torqueOn(id);
 }
