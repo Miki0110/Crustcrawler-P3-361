d7955e76b65117900df5dd3c31f6dfaf858995dd
SimonHaaningAAU <san20@student.aau.dk>
2021-11-04 10:05:19 +0100

    Torque constant added + debug serial message to other arduino

--- CommandMotorMovement/CommandMotorMovement.ino
+++ CommandMotorMovement/CommandMotorMovement.ino
@@ -1,6 +1,9 @@
 #include <DynamixelShield.h>
 #include <SoftwareSerial.h>
 
+SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RX/TX
+#define DEBUG_SERIAL soft_serial
+
 //Motor IDs
 const uint8_t DXL_ID[6] = {0, 1, 2, 3, 4, 5};
 
@@ -10,36 +13,33 @@ DynamixelShield dxl;
 //Namespace for motor control table entries
 using namespace ControlTableItem;
 
-//Returns current position as radiants
-float getMotorPositions(uint8_t id) {
-  dxl.get
+//Returns joint angle as radiants
+float getMotorPosition(uint8_t id) {
+  return PI/180 * dxl.getPresentPosition(id, UNIT_DEGREE);
 }
 
-//TODO find torque constants
-//Sets current from torque profile
+//Returns joint velocity in radiants/s
+float getMotorVelocity(uint8_t id) {
+  return (1/60) * 2*PI * dxl.getPresentVelocity(id, UNIT_RPM);
+}
+
+//Sets current from torque
 boolean setMotorTorque(uint8_t id, float torque) {
+  float current;
   switch(id){
     case 1: //MX-64
     case 3:
-      float MX64Kt; //Torque constant for MX-64 motors
-      float current = (torque / MX64Kt) / 1000; //Calculate current from torque (in milliamperes)
+      float MX64Kt = 1.8189; //Torque constant for MX-64 motors
+      current = (torque / MX64Kt) / 1000; //Calculate current from torque (in milliamperes)
       dxl.setGoalCurrent(id, current, UNIT_MILLI_AMPERE);
       return 1;
     
     case 2: //MX-106
-      float MX106Kt; //Torque constant for MX-106 motors
-      float current = (torque / MX106Kt) / 1000; //Calculate current from torque (in milliamperes)
+      float MX106Kt = 2.5465; //Torque constant for MX-106 motors
+      current = (torque / MX106Kt) / 1000; //Calculate current from torque (in milliamperes)
       dxl.setGoalCurrent(id, current, UNIT_MILLI_AMPERE);
       return 1;
-
-    /* Cannot run in torque mode
-    case 4: //MX-28
-    case 5:
-      float MX28Kt; //Torque constant for MX-28 motors
-      float current = (torque / MX28Kt) / 1000; //Calculate current from torque (in milliamperes)
-      dxl.setGoalCurrent(id, current, UNIT_MILLI_AMPERE);
-      return 1;
-    */
+      
     default:
       //Error wrong id
       return 0;
@@ -48,27 +48,20 @@ boolean setMotorTorque(uint8_t id, float torque) {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-
   // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
   dxl.begin(57600);
   // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
-  
-  //Set maximum current (percentage)
-  //setCurrentLimit(DXL_ID[1], 0);
-  //setCurrentLimit(DXL_ID[2], 1000);//Range 0 - 1941
-  //setCurrentLimit(DXL_ID[3], 0);
-  //setCurrentLimit(DXL_ID[4], 0);
-  //setCurrentLimit(DXL_ID[5], 0);
+
+  //Begins serial for debug and testing
+  Serial1.begin(57600);
+  while(!Serial1); //Wait for serial port to be available
 
   //Orient arm
-  //startupPosition(DXL_ID[2]);
-  //startupPosition(DXL_ID[3]);
-  //dxl.setGoalPosition(DXL_ID[2], 4095/2);
-  //dxl.setGoalPosition(DXL_ID[3], 4095/2);
-
-  //delay(1000);
+  startupPosition(DXL_ID[2]);
+  startupPosition(DXL_ID[3]);
+  dxl.setGoalPosition(DXL_ID[2], 90, UNIT_DEGREE);
+  dxl.setGoalPosition(DXL_ID[3], 180, UNIT_DEGREE);
   
   //Initialise motor control modes
   //startupCurrent(DXL_ID[1]);
@@ -78,7 +71,32 @@ void setup() {
   //startupPosition(DXL_ID[5]);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+float desiredCurrent  = 0; //Unit 3.36mA
+float desiredVelocity = 0; //Unit 0.229rpm
+float desiredPosition = 0; //Unit 0.088deg
 
+const int millisBetweenDataSend = 10;
+unsigned long currentDataSendMillis = 0;
+
+void loop() {
+  //Information to debug arduino
+
+  if(millis() >= currentDataSendMillis + millisBetweenDataSend) {
+    int cur = (int)(dxl.getPresentCurrent(DXL_ID[2]) * 100);
+    int vel = dxl.getPresentVelocity(DXL_ID[2]);
+    int pos = dxl.getPresentPosition(DXL_ID[2]);
+    
+    Serial1.write(0x7E);
+    Serial1.write(highByte(cur));
+    Serial1.write(lowByte(cur));
+    Serial1.write(highByte(vel));
+    Serial1.write(lowByte(vel));
+    Serial1.write(highByte(pos));
+    Serial1.write(lowByte(pos));
+    currentDataSendMillis = millis();
+  }
+
+  //control loop for motor
+  
+  
 }
