017c6f0cc4bb404842372ee9ea7cdd40a4967743
FrederikNiel <fsni20@student.aau.dk>
2021-11-17 11:40:42 +0100

    Created thresholdDeterminer with sample code

---
+++ sEMGSerialReaderFunction/thresholdDeterminer.ino
@@ -0,0 +1,16 @@
+/* Function for comparing the fathced sEMG arrays with a designated threshold
+ * and detimine if the signal is a blink/hold.
+ *  
+ * This function requires sEMGch1.getAverage() and sEMGch2.getAverage()  
+ * to be running and thir data declared in an array
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
