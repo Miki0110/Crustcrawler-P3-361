af0cd797c12bb0c0e6599ff85dede6af58cab89d
FrederikNiel <fsni20@student.aau.dk>
2021-11-18 09:41:01 +0100

    Small changes with code

--- sEMGSerialReaderFunction/thresholdDeterminer.ino
+++ sEMGSerialReaderFunction/thresholdDeterminer.ino
@@ -4,17 +4,29 @@ int Threshold2 = 120; //Frontalis 75%
 boolean sEMGfinal1 = 0;
 boolean sEMGfinal2 = 0;
 
-int getValue [2](){
+bool FinalValues[20];
+
+
+
+
+
+void thresHold(boolean getAverage){
+  
+//int getValue [2](){
 if(sEMGch1.getAverage() >= Threshold1){sEMGfinal1=1;} // This used to cumper with the thresholdes
-else{sEMGfinl1=0;}
+else{sEMGfinal1=0;}
 
 
 
 if(sEMGch2.getAverage() >= Threshold2){sEMGfinal2=1;}// This used to cumper with the thresholdes
 else{sEMGfinal2=0;}
 }
+//Here the booleans should be added to the array
 
-int FinalValues [2]={sEMGfinal1,sEMGfinal2};
 
-return FinalValues;
+//sEMGfinal1,sEMGfinal2};
+//Serial.println(FinalValues)
+
+
+return true;
 }
