6e828d4e70d920a21a61094b750e438814a1e0ed
FrederikNiel <fsni20@student.aau.dk>
2021-11-23 08:35:25 +0100

    sEMGInterpreter partially works, signalling axis + and -

--- sEMGSerialReaderFunction/sEMGInterpreter.ino
+++ sEMGSerialReaderFunction/sEMGInterpreter.ino
@@ -9,24 +9,27 @@ void sEMGMessegeGenerater() {
   if (sEMGch1.getAverage() >= Threshold1) {
     sEMGch1HeldTime++;
 
+    sEMGch1WaitingForMessage = 0;
   }
 
-  else if (sEMGch1.getAverage() < Threshold1) {
-    sEMGch1HeldTime = 0;
+  else if (sEMGch1.getAverage() < Threshold1 && !sEMGch1WaitingForMessage) {
+    sEMGNewMessage = 1;
 
-    sEMGch1NewMessege = 1;
+    sEMGch1WaitingForMessage = 1;
 
   }
 
   if (sEMGch2.getAverage() >= Threshold2) {
     sEMGch2HeldTime++;
 
+    sEMGch2WaitingForMessage = 0;
   }
 
-  else if (sEMGch2.getAverage() < Threshold2) {
-    sEMGch2HeldTime = 0;
+  else if (sEMGch2.getAverage() < Threshold2 && !sEMGch2WaitingForMessage) {
+    sEMGNewMessage = 1;
 
-    sEMGch2NewMessege = 1;
+    sEMGch2WaitingForMessage = 1;
+
 
   }
 
@@ -34,9 +37,9 @@ void sEMGMessegeGenerater() {
 }
 // CHanges to function: Switchcase here
 void sEMGInterpreter() {
-  unsigned char signalArray[2];
+  unsigned int signalArray[2] = {0, 0};
 
-  if (sEMGch1NewMessege = 1) {
+  if (sEMGNewMessage == 1) {
     if (sEMGch1HeldTime >= timeForHold) {
       signalArray[0] = 2;
     }
@@ -44,7 +47,6 @@ void sEMGInterpreter() {
       signalArray[0] = 1;
     }
 
-    sEMGch1NewMessege = 0;
     sEMGch1HeldTime = 0;
 
     if (sEMGch2HeldTime >= timeForHold) {
@@ -54,26 +56,38 @@ void sEMGInterpreter() {
       signalArray[1] = 1;
     }
 
-    sEMGch2NewMessege = 0;
+    sEMGNewMessage = 0;
     sEMGch2HeldTime = 0;
 
   }
+
   if (signalArray[0] == 1 && signalArray[1] == 1) {
     //Cycle Axis
-    Serial.println("Cycle Axis" ",");
+    Serial.println("Cycle Axis " ",");
   }
   else if (signalArray[0] == 2 && signalArray[1] == 0) {
     //Axis+
-    Serial.print("Axis");
+    Serial.print("Axis +");
   }
   else if (signalArray[0] == 0 && signalArray[1] == 2) {
     //Axis-
-    Serial.print("Axis");
+    Serial.print("Axis -");
   }
   else if (signalArray[0] == 2 && signalArray[1] == 2) {
     //Gripper Toggel
-    Serial.print("Gripper Toggel");
+    Serial.println("Gripper Toggel");
   }
   //else if (signalArray[0] == 2 && signalArray[1] == 0) {
   //Emargncy Stop
+  /*
+    if (sEMGch1NewMessage==0&&sEMGch2NewMessage==0){
+      signalArray[0]=0;
+      signalArray[1]=0;
+      //Serial.println(signalArray[1]);
+      //Serial.println(signalArray[0]);
+      }
+
+  */
+
+
 }// end void
--- sEMGSerialReaderFunction/sEMGSerialReaderFunction.ino
+++ sEMGSerialReaderFunction/sEMGSerialReaderFunction.ino
@@ -1,20 +1,20 @@
 /* Function to fetch data recieved with XBee from the sEMG module
- * 
- * XBee must be connected to Serial1 port on Arduino Mega 2560
- * 
- * Function requires sEMGFetchedData integer array of 5 elements to be defined before calling function
- * Data will be stored in array 
- */
+
+   XBee must be connected to Serial1 port on Arduino Mega 2560
+
+   Function requires sEMGFetchedData integer array of 5 elements to be defined before calling function
+   Data will be stored in array
+*/
 
 
-int Threshold1 = 135; //Orbiclaris oculi 75% = 135
-int Threshold2 = 120; //Frontalis 75% = 120
+int Threshold1 = 535; //Orbiclaris oculi 75% = 135
+int Threshold2 = 520; //Frontalis 75% = 120
 
 bool sEMGfinal1 = 0; //Global for when threshold reached for channel one
 bool sEMGfinal2 = 0; //Global for when threshold reached for channel two
 
 bool FinalValues[2]; //Array for boolean data for thresholds
- 
+
 int sEMGFetchedData[5]; //Array for storing newest data fetched from sEMG
 
 int sEMGInterpreterSampleTime = 10; //Time between interpreter mesurements in miliseconds
@@ -24,11 +24,12 @@ unsigned long sEMGInterpreterTime;
 unsigned int sEMGch1HeldTime; //Time the channel has held time with unit: sEMGInterpreterSampleTime in ms
 unsigned int sEMGch2HeldTime; //-||-
 
-unsigned int timeForHold = 200; //Time for impulse to be hold, unit is sEMGInterpreterSampleTime in ms
+unsigned int timeForHold = 100; //Time for impulse to be hold, unit is sEMGInterpreterSampleTime in ms
 
-bool sEMGch1NewMessege;
-bool sEMGch2NewMessege;
+bool sEMGNewMessage;
 
+bool sEMGch1WaitingForMessage = 1;
+bool sEMGch2WaitingForMessage = 1;
 
 //Running average library by Rob Tillaart
 #include <RunningAverage.h>
@@ -39,15 +40,15 @@ RunningAverage sEMGch1(25);
 RunningAverage sEMGch2(25);
 
 
-void setup() 
+void setup()
 {
   //Should be added to setup of arduino program calling this function
   Serial1.begin(115200);//Start serial port for recieving message from XBee
   while (!Serial1) {}   //Wait for serial port to be active
-   Serial.begin(115200);//Start serial port for recieving message from XBee
+  Serial.begin(115200);//Start serial port for recieving message from XBee
   while (!Serial) {}   //Wait for serial port to be active
 
-  
+
   //Ensure clean slate for all rolling averages
   sEMGch1.clear();
   sEMGch2.clear();
@@ -56,9 +57,9 @@ void setup()
 
 }
 
+unsigned long debugMillis;
 
-
-void loop() 
+void loop()
 {
   //Fetch data
   fetchDataFromsEMG(100);
@@ -67,23 +68,29 @@ void loop()
   sEMGch1.addValue(sEMGFetchedData[3]);
   sEMGch2.addValue(sEMGFetchedData[4]);
 
-  if (millis()>=sEMGInterpreterTime+sEMGInterpreterSampleTime){
-    //fkt
-    sEMGInterpreterTime = millis();    
+  if (millis() >= sEMGInterpreterTime + sEMGInterpreterSampleTime) {
+    sEMGMessegeGenerater();
+    sEMGInterpreterTime = millis();
   }
-
+  if (millis() >= debugMillis + 500) {
+    Serial.print(sEMGch1.getAverage(), 0);
+    Serial.print(", ");
+    Serial.println(sEMGch2.getAverage(), 0);
+    debugMillis = millis();
+  }
 
   //Print to serial plotter/monitor
   //Serial.print(sEMGFetchedData[3]);
   //Serial.print(", ");
   //Serial.print(sEMGFetchedData[4]);
   //Serial.print(", ");
-  Serial.print(sEMGch1.getAverage(), 0);
-  Serial.print(", ");
-  Serial.println(sEMGch2.getAverage(), 0);
+  //Serial.print(sEMGch1.getAverage(), 0);
+  //Serial.print(", ");
+  //Serial.println(sEMGch2.getAverage(), 0);
 
   //Calling BthresholdDetminer which gives FinalValues[2] 1 if signal is over threshold.
   thresHold();
 
+  sEMGInterpreter();
 
 }
