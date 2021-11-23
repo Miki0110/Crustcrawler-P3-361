dee38cfa42f4e8ebe0c83f673f3faeaf572b79a3
SimonHaaningAAU <san20@student.aau.dk>
2021-11-23 14:37:48 +0100

    Fixed mistake in merge

--- sEMGSerialReaderFunction/sEMGSerialReaderFunction.ino
+++ sEMGSerialReaderFunction/sEMGSerialReaderFunction.ino
@@ -1,78 +1,72 @@
 /* Function to fetch data recieved with XBee from the sEMG module
- * 
- * XBee must be connected to Serial1 port on Arduino Mega 2560
- * 
- * Function requires sEMGFetchedData integer array of 5 elements to be defined before calling function
- * Data will be stored in array 
- */
+
+   XBee must be connected to Serial1 port on Arduino Mega 2560
+
+   Function requires sEMGFetchedData integer array of 5 elements to be defined before calling function
+   Data will be stored in array
+*/
+
+int Threshold1 = 535; //Orbiclaris oculi 75% = 135
+int Threshold2 = 520; //Frontalis 75% = 120
+
+bool sEMGfinal1 = 0; //Global for when threshold reached for channel one
+bool sEMGfinal2 = 0; //Global for when threshold reached for channel two
+
 int sEMGFetchedData[5]; //Array for storing newest data fetched from sEMG
 
-void setup() 
+int sEMGInterpreterSampleTime = 10; //Time between interpreter mesurements in miliseconds (minimum should be 10)
+unsigned long sEMGInterpreterTime;  //Counter to keep track of milliseconds between the interpreiter sample collection
+
+unsigned int timeForHold = 100; //Time for impulse to be held, unit is sEMGInterpreterSampleTime in ms
+
+//Running average library by Rob Tillaart
+#include <RunningAverage.h>
+
+//Define rolling averages and sample amounts (keep sample amounts low (<50))
+RunningAverage sEMGch1(25);
+RunningAverage sEMGch2(25);
+
+void setup()
 {
   //Should be added to setup of arduino program calling this function
   Serial1.begin(115200);//Start serial port for recieving message from XBee
   while (!Serial1) {}   //Wait for serial port to be active
-   Serial.begin(115200);//Start serial port for recieving message from XBee
+  Serial.begin(115200);//Start serial port for recieving message from XBee
   while (!Serial) {}   //Wait for serial port to be active
+
+  //Ensure clean slate for all rolling averages
+  sEMGch1.clear();
+  sEMGch2.clear();
+
+  //Prepare interpreiter timer for use
+  sEMGInterpreterTime = millis();
 }
 
-//Averaging of signal adjust numReadings and readings index to alter smoothness level and responsiveness
-int numReadings = 50;
-int readIndex = 0;
-long total = 0;
-int readings[50];
+//Debug
+//unsigned long debugMillis;
 
-long smooth() {
-  long average;
-  total = total - readings[readIndex];
-  readings[readIndex] = sEMGFetchedData[4];
-  total = total + readings[readIndex];
-  readIndex ++;
-  if(readIndex >= numReadings) {
-    readIndex = 0;
-  }
-  average = total/numReadings;
-  return average;
-}
-
-void loop() 
+void loop()
 {
-  
-  fetchDataFromsEMG (100);
-  Serial.println(smooth());
-}
+  //Fetch data
+  fetchDataFromsEMG(100);
 
-//Call function to fetch data from sEMG
-//Alters the content of the sEMGFetchedData array
-//Returns true if data was fetched successfully, otherwise returns false
-bool fetchDataFromsEMG(int timeOutAfter)
-{
-  byte fullMessageFromsEMG[24]; //Byte array for storing full wrapped message
-  long currentMillis = millis();
-  
-  //Loop until message is recieved
-  while(true) {
-    if (currentMillis + timeOutAfter <= millis()) //Timeout if specified time in milliseconds is reached
-    {
-      return false; //Failed to recieve message (Timed out)
-    }
-    
-    if (Serial1.read() == 0x7E) //Check serial bitstream for start bit 0x7E
-    { 
-      Serial1.readBytes(fullMessageFromsEMG, 23); //Record following bits into the message array
-  
-      for (int i = 23; i > 0; i--)  //Rearrange array to make room for start bit in message
-      {
-        fullMessageFromsEMG[i] = fullMessageFromsEMG[i-1];
-      }
-      fullMessageFromsEMG[0] = 0x7E;//Add start bit to message
+  //Add data to rolling averages
+  sEMGch1.addValue(sEMGFetchedData[3]);
+  sEMGch2.addValue(sEMGFetchedData[4]);
 
-      for (int i = 22; i >= 14; i -= 2) //Write data to sEMGFetchedData
-      { 
-        //Combine msb and lsb into single variable
-        sEMGFetchedData[(i - 14)/2] = fullMessageFromsEMG[i] + (fullMessageFromsEMG[i - 1] << 8);
-      }
-      return true;//Recieved message
-    }
+  //Run signal interpreiter
+  if (millis() >= sEMGInterpreterTime + sEMGInterpreterSampleTime) {
+    sEMGInterpreter();
+    sEMGInterpreterTime = millis();
   }
+  
+  //Debug
+  /*
+    if (millis() >= debugMillis + 500) {
+    Serial.print(sEMGch1.getAverage(), 0);
+    Serial.print(", ");
+    Serial.println(sEMGch2.getAverage(), 0);
+    debugMillis = millis();
+    }
+  */
 }
