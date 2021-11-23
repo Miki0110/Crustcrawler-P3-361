8d175cabd951033563e2435ca2d8413026de9554
FrederikNiel <fsni20@student.aau.dk>
2021-11-23 14:14:41 +0100

    Revert "Merge branch 'sEMGSerialReaderFunctionWithTimer'"
    This reverts commit 807172ded46de9b9e73ef7b38c787f33204fd5ab, reversing
    changes made to bf185f2b17567bbbf6f0b3b1422978801623a507.

--- sEMGSerialReaderFunction/sEMGSerialReaderFunction.ino
+++ sEMGSerialReaderFunction/sEMGSerialReaderFunction.ino
@@ -5,44 +5,8 @@
  * Function requires sEMGFetchedData integer array of 5 elements to be defined before calling function
  * Data will be stored in array 
  */
-//Wellats globals------------------------------------------------------------------------------------------------------------------------------------
-int Fvloop[]={0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0}; //simalter
-int change = 0; //simalter
-int OldFinalValues[10];
-int impuls1 = 0;
-int impuls2 = 0;
-int var;
- //-----------------------------------------------------------------------------------------------------------------------------------------------
-int Threshold1 = 135; //Orbiclaris oculi 75% = 135
-int Threshold2 = 120; //Frontalis 75% = 120
-
-bool sEMGfinal1 = 0; //Global for when threshold reached for channel one
-bool sEMGfinal2 = 0; //Global for when threshold reached for channel two
-
-bool FinalValues[2]; //Array for boolean data for thresholds
- 
 int sEMGFetchedData[5]; //Array for storing newest data fetched from sEMG
 
-int triggerPointch1 = 800;  //Trigger point for sEMG channel 1
-int triggerPointch2 = 800;  //Trigger point for sEMG channel 2
-
-
-//Running average library by Rob Tillaart
-#include <RunningAverage.h>
-
-
-//Define rolling averages and sample amounts (keep sample amounts low (<50))
-RunningAverage sEMGch1(25);
-RunningAverage sEMGch2(25);
-
-//Wellats timer
-/*---------------------Timer-----------------------------*/
-RunningAverage totalAverage(2); //add data here
-int sampleTime = 1*1000; //add seconds here
-int totalAverageCounter = 1;
-unsigned long currentMillis;
-/*---------------------Timer------------------------------------------------------------------------------------------------------------------*/
-//
 void setup() 
 {
   //Should be added to setup of arduino program calling this function
@@ -50,51 +14,32 @@ void setup()
   while (!Serial1) {}   //Wait for serial port to be active
    Serial.begin(115200);//Start serial port for recieving message from XBee
   while (!Serial) {}   //Wait for serial port to be active
+}
 
-  //Testing
-  pinMode(52, OUTPUT);
-  pinMode(53, OUTPUT);
+//Averaging of signal adjust numReadings and readings index to alter smoothness level and responsiveness
+int numReadings = 50;
+int readIndex = 0;
+long total = 0;
+int readings[50];
 
-  //Ensure clean slate for all rolling averages
-  sEMGch1.clear();
-  sEMGch2.clear();
+long smooth() {
+  long average;
+  total = total - readings[readIndex];
+  readings[readIndex] = sEMGFetchedData[4];
+  total = total + readings[readIndex];
+  readIndex ++;
+  if(readIndex >= numReadings) {
+    readIndex = 0;
+  }
+  average = total/numReadings;
+  return average;
 }
 
 void loop() 
 {
-  //Fetch data
-  fetchDataFromsEMG(100);
-
-  //Add data to rolling averages
-  sEMGch1.addValue(sEMGFetchedData[3]);
-  sEMGch2.addValue(sEMGFetchedData[4]);
-
-  //Print to serial plotter/monitor
-  //Serial.print(sEMGFetchedData[3]);
-  //Serial.print(", ");
-  //Serial.print(sEMGFetchedData[4]);
-  //Serial.print(", ");
-  //Serial.print(sEMGch1.getAverage(), 0);
-  //Serial.print(", ");
-  //Serial.println(sEMGch2.getAverage(), 0);
-
-  //Calling BthresholdDetminer which gives FinalValues[2] 1 if signal is over threshold.
-  thresHold();
-
-    //Print FinalValues array to serial monitor - only for debugging
-    //Serial.println(FinalValues[1]);
-    //Serial.println(FinalValues[2]);
-
-
-  sampleTimeVal();
-
-/*
-  //Testing set pins to high when threshold is met - Can we delete this?
-  if(     sEMGch1.getAverage() >= triggerPointch1) digitalWrite(52, HIGH);
-  else if(sEMGch1.getAverage() <  triggerPointch1) digitalWrite(52,  LOW);
-  if(     sEMGch2.getAverage() >= triggerPointch2) digitalWrite(52, HIGH);
-  else if(sEMGch2.getAverage() <  triggerPointch2) digitalWrite(52,  LOW);
-  */
+  
+  fetchDataFromsEMG (100);
+  Serial.println(smooth());
 }
 
 //Call function to fetch data from sEMG
