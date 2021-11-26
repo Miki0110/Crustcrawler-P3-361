96d93b5acb23f8b4e82ccf48566acfa0fa59dd44
gsvend20 <gsvend20@student.aau.dk>
2021-11-26 10:34:21 +0100

    Modified LCD to print Axis and Loop time

--- CommandMotorMovement/LCD.ino
+++ CommandMotorMovement/LCD.ino
@@ -1,4 +1,4 @@
-
+//This is for an LCD using an I2C adapter
 void LCDPrint(){
   lcd.clear();
 int TotalStringLenght=0;
@@ -6,6 +6,7 @@ String PrintString="";
 
   lcd.setCursor(0,0); //Print on first line
   lcd.print("Current Axis: ");
+  lcd.setCursor(14,0);
   lcd.print(currentAxis);
   Serial.println(currentAxis);
 
@@ -13,7 +14,9 @@ String PrintString="";
 //lcd.print(TotalStringLenght/2 + PrintString);
 
   lcd.setCursor(0,1); //Print on second line
-lcd.print("1 Loop Run Time: ");
+lcd.print("Time: ");
+  lcd.setCursor(6,1);
 lcd.print(millis()-lastCalcTime);
+  lcd.setCursor(13,1);
 lcd.print(" ms");
   }
