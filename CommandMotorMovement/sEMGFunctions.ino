13ae6ceb71070488c0653ab8ffe858cba9b43d18
SimonHaaningAAU <san20@student.aau.dk>
2021-11-29 11:51:15 +0100

    Reworked interpreter logic for sEMG commands.

--- CommandMotorMovement/sEMGFunctions.ino
+++ CommandMotorMovement/sEMGFunctions.ino
@@ -70,10 +70,6 @@ bool fetchDataFromsEMG(int timeOutAfter) {
   }
 }
 
-//Add sEMG data to rolling averages
-//sEMGch1.addValue(sEMGFetchedData[3]);
-//sEMGch2.addValue(sEMGFetchedData[4]);
-
 /*Following function interpreits a given command from sEMG channels
   Thresholds are set in main document as global variables for easier tuning
 */
@@ -82,86 +78,48 @@ bool fetchDataFromsEMG(int timeOutAfter) {
 unsigned int sEMGch1HeldTime = 0;
 unsigned int sEMGch2HeldTime = 0;
 
-//Variables to remember the held time of last cycle
-unsigned int sEMGch1LastHeldTime = 0;
-unsigned int sEMGch2LastHeldTime = 0;
-
 //Remember if gripper was just toggled
 bool blockGripperToggle = false;
 
 //sEMG interpreter function
 void sEMGInterpreter() {
-  //Command signal type recieved from sEMG channels
-  //0 = no signal, 1 = impulse signal, 2 = long signal
-  byte sEMGch1CommandSignal = 0;
-  byte sEMGch2CommandSignal = 0;
 
   //If signal is held above the threshold the respective counter should be incremented
-  if (sEMGch1.getAverage() >= sEMGch1Threshold) {
+  if (sEMGch1.getAverage() >= sEMGch1Threshold) { //Signal active
     sEMGch1HeldTime++;
-  }
-  if (sEMGch2.getAverage() >= sEMGch2Threshold) {
-    sEMGch2HeldTime++;
-  }
-
-  //Determine what type of signal is sent on channel 1
-  if (sEMGch1HeldTime <= sEMGch1LastHeldTime) {
-    //Signal ended or not started
-    if (sEMGch1HeldTime >= timeForHold) {
-      bool blockGripperToggle = false;
+    
+    if (sEMGch1HeldTime >= timeForHold && sEMGch2HeldTime == 0) { //Held signal underway
+      interpretedCommand = 2; //Axis+
     }
-    else if (sEMGch1HeldTime > 0) {
-      sEMGch1CommandSignal = 1; //Impulse signal recieved
+  }
+  else { //Signal inactive
+    if (sEMGch1HeldTime >= 1 && sEMGch1HeldTime <= timeForHold && sEMGch2HeldTime == 0) {
+      interpretedCommand = 1; //Cycle Axis
     }
     sEMGch1HeldTime = 0;//Reset held time
   }
-  else {
-    //Signal still underway
-    if (sEMGch1HeldTime >= timeForHold) {
-      sEMGch1CommandSignal = 2; //Held signal recieved
+  
+  if (sEMGch2.getAverage() >= sEMGch2Threshold) { //Signal active
+    sEMGch2HeldTime++;
+    
+    if (sEMGch2HeldTime >= timeForHold && sEMGch1HeldTime == 0) { //Held signal underway
+      interpretedCommand = 3; //Axis-
     }
   }
-  sEMGch1LastHeldTime = sEMGch1HeldTime;//Record held time for channel 1
+  else { //Signal inactive
+    sEMGch2HeldTime = 0;//Reset held time
+  }
 
-  //Determine what type of signal is sent on channel 2
-  if (sEMGch2HeldTime <= sEMGch2LastHeldTime) {
-    //Signal ended or not started
-    if (sEMGch2HeldTime >= timeForHold) {
-      bool blockGripperToggle = false;
-    }
-    else if (sEMGch2HeldTime > 0) {
-      sEMGch2CommandSignal = 1; //Impulse signal recieved
-    }
-    sEMGch2HeldTime = 0; //Reset held time
-  }
-  else {
-    //Signal still underway
-    if (sEMGch2HeldTime >= timeForHold) {
-      sEMGch2CommandSignal = 2; //Held signal recieved
-    }
-  }
-  sEMGch2LastHeldTime = sEMGch2HeldTime;//Record held time for channel 2
+  //Print Held time for debug
+  //Serial.print(sEMGch1HeldTime);
+  //Serial.print(" ");
+  //Serial.println(sEMGch2HeldTime);
 
-  //From both channel signals determine what command was sent
-  if (sEMGch1CommandSignal == 1) {
-    //Cycle Axis
-    interpretedCommand = 1;
-  }
-  //else if (sEMGch2CommandSignal == 1) {
-  //Emergency axis
-  //  Serial.println("E-stop");
-  //}
-  else if (sEMGch1CommandSignal == 2 && sEMGch2CommandSignal == 0) {
-    //Axis+
-    interpretedCommand = 2;
-  }
-  else if (sEMGch1CommandSignal == 0 && sEMGch2CommandSignal == 2) {
-    //Axis-
-    interpretedCommand = 3;
-  }
-  else if (sEMGch1CommandSignal == 2 && sEMGch2CommandSignal == 2) {
-    //Gripper Toggle
-    interpretedCommand = 4;
-    blockGripperToggle = true;
+  if (sEMGch1HeldTime >= timeForHold && sEMGch2HeldTime >= timeForHold) {
+    interpretedCommand = 4; //Gripper Toggle
+
+    //Reset everything to avoid clapping
+    sEMGch1HeldTime = 0;//Reset held time
+    sEMGch2HeldTime = 0;//Reset held time
   }
 }
